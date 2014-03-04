e773ca8553224177bb243693d1aac245a76a04d8
sameeram <mgjaye@gmail.com>
2014-03-04 19:22:40 +0530

    Moved the entity and utils to modules

---
+++ jaggery-utils/scripts/reflection/reflection.js
@@ -0,0 +1,64 @@
+var reflection = {};
+
+/**
+ * Description: The script encapsulates any reflection related utility functions
+ */
+(function () {
+
+    var log = new Log('utils-reflection');
+
+    reflection.copyPropKeys = function (from, to) {
+        for (var key in from) {
+            if (from.hasOwnProperty(key)) {
+                to[key] = '';
+            }
+        }
+
+        return to;
+    };
+
+    reflection.copyProps = function (from, to) {
+        for (var key in from) {
+            if (from.hasOwnProperty(key)) {
+                to[key] = from[key];
+            }
+        }
+
+        return to;
+    };
+
+    reflection.getProps = function (obj) {
+        var props={};
+
+        for(var key in obj){
+            if(!(obj[key] instanceof  Function)){
+                props[key]=obj[key];
+            }
+        }
+
+        return props;
+    };
+
+
+    reflection.printProps = function (obj) {
+        for (var key in obj) {
+            if (obj.hasOwnProperty(key)) {
+                log.info('key: ' + key);
+            }
+        }
+    };
+
+    var getDiff=function(a,b,diff){
+
+    };
+
+    /**
+     * The function calculates the differences between two simple JSON objects
+     * @param a  The object with which b is compared
+     * @param b  The target of the comparison
+     * @return An object which records the differences between the two objects
+     */
+    reflection.diff=function(a,b){
+
+    };
+
